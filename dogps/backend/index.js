bb2df659146c91948ec22a08cd3e760b87acb7bb
Ryan Dewey <145182587+RyanDewey@users.noreply.github.com>
2026-02-11 17:30:47 -0800

    Fixed backend import problems

--- dogps/backend/index.js
+++ dogps/backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dogRouter from "./routes/dogRoutes";
+import dogRouter from "./routes/dogRoutes.js";
 
 // Create app instance
 const app = express();
--- dogps/backend/routes/dogRoutes.js
+++ dogps/backend/routes/dogRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getAdoptableDogs } from "../services/dogService";
+import { getAdoptableDogs } from "../services/dogService.js";
 
 router.get('/', async (req, res) => {
     const dogs = await getAdoptableDogs({ limit: 30 });
     res.send(dogs);
 })
-
 
 export default router;
 
